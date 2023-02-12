aac6d952cc544e8fffbfba1242cd4f50b345a5ca
Ifteshawn <ifteshawn@gmail.com>
2023-02-12 19:47:16 +1000

    Styled card component. Input dummy content into card

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -6,18 +6,22 @@ import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles((theme) => ({
   root: {
     "& .MuiPaper-root": {
-      backgroundColor: 'transparent',
+      backgroundColor: "transparent",
       // backgroundColor: '#8186D5',
-      backdropFilter: 'blur(1px)'
+      backdropFilter: "blur(1px)",
     },
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '84.5vh',
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "84.5vh",
   },
   card: {
-    width: '80%',
-    maxWidth: 700,
+    width: "80%",
+    height: "60%",
+    // maxWidth: 700,
+    minHeight: 300,
+    display: 'grid',
+    alignContent: 'stretch'
   },
 }));
 
@@ -25,12 +29,30 @@ export default function ContainerCard() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Card
-        variant="outlined"
-        className={classes.card} >
-        <CardContent >
-          <p>This is a sample card with a backdrop filter effect applied to its
-            container.</p>
+      <Card variant="outlined" className={classes.card}>
+        <CardContent
+          style={{ display: "flex", justifyContent: "space-evenly" }}
+        >
+          <div style={{display:'flex', flexDirection: "column", justifyContent:'center'}}>
+            <img src="http://openweathermap.org/img/wn/10d@2x.png" />
+            <h3>-12°C</h3>
+            <h4>Snow storm</h4>
+            <strong>Rockhampton, QLD</strong>
+          </div>
+          <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly" }}>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <label>City: </label>
+              <input type="text" placeholder="Enter a City name here."></input>
+            </div>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <label>Country: </label>
+              <input
+                type="text"
+                placeholder="Enter a Country name here."
+              ></input>
+            </div>
+            <button>Search</button>
+          </div>
         </CardContent>
       </Card>
     </div>
