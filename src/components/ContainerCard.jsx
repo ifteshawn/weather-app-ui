98d864ebb84a4ea0e953088995de1cb2a4a3147e
Ifteshawn <ifteshawn@gmail.com>
2023-02-15 20:58:48 +1000

    Implemented weather conditions search functionality

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "@mui/material/Card";
 import { CardContent } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,38 +21,89 @@ const useStyles = makeStyles((theme) => ({
     height: "60%",
     // maxWidth: 700,
     minHeight: 300,
-    display: 'grid',
-    alignContent: 'stretch'
+    display: "grid",
+    alignContent: "stretch",
   },
 }));
 
 export default function ContainerCard() {
   const classes = useStyles();
+  const [weather, setWeather] = useState("");
+
+  const baseUrl =
+    "https://localhost:7061/api/WeatherInformation/getWeatherInfo/";
+
+  function handleSearch() {
+    const city = document.getElementById("cityName").value;
+
+    fetch(
+      `${baseUrl}${city}`
+      // , {
+      //   headers: {
+      //     "Authorization": "Bearer 8b7535b42fe1c551f18028f64e8688f7",
+      //     "APIKey": "8b7535b42fe1c551f18028f64e8688f7"
+      //   },
+      // }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const weather = {
+          city: data.city,
+          temp: data.temp,
+          description: data.description,
+          iconSrc:
+            "http://openweathermap.org/img/wn/" + `${data.icon}` + "@2x.png",
+        };
+        // console.log(weather);
+        setWeather(weather);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
   return (
     <div className={classes.root}>
       <Card variant="outlined" className={classes.card}>
         <CardContent
           style={{ display: "flex", justifyContent: "space-evenly" }}
         >
-          <div style={{display:'flex', flexDirection: "column", justifyContent:'center'}}>
-            <img src="http://openweathermap.org/img/wn/10d@2x.png" />
-            <h3>-12°C</h3>
-            <h4>Snow storm</h4>
-            <strong>Rockhampton, QLD</strong>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <img src={weather.iconSrc} alt="Current weather icon." />
+            <h3>{weather.temp}°C</h3>
+            <h4>{weather.description}</h4>
+            <strong>{weather.city}</strong>
           </div>
-          <div style={{ display: "flex", flexDirection: "column", justifyContent: "space-evenly" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-evenly",
+            }}
+          >
             <div style={{ display: "flex", justifyContent: "space-between" }}>
               <label>City: </label>
-              <input type="text" placeholder="Enter a City name here."></input>
+              <input
+                id="cityName"
+                type="text"
+                placeholder="Enter a City name here."
+              ></input>
             </div>
             <div style={{ display: "flex", justifyContent: "space-between" }}>
               <label>Country: </label>
               <input
+                id="countryName"
                 type="text"
                 placeholder="Enter a Country name here."
               ></input>
             </div>
-            <button>Search</button>
+            <button onClick={handleSearch}>Search</button>
           </div>
         </CardContent>
       </Card>
