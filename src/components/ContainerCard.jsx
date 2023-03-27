e341a09376a3cf258f2d11da8df29b0856f297ba
Ifteshawn <ifteshawn@gmail.com>
2023-03-27 20:49:00 +1000

    Refactor ContainerCard, move fetch function to a separate module

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -1,23 +1,18 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import Card from "@mui/material/Card";
-import { CardContent } from "@mui/material";
+import CardContent from "@mui/material/CardContent";
 import { makeStyles } from "@mui/styles";
+import { DotLoader } from "react-spinners";
 import WeatherInfoArea from "./WeatherInfoArea";
 import SearchArea from "./SearchArea";
 import ErrorInfo from "./ErrorInfo";
-import { DotLoader } from "react-spinners";
+import { fetchWeather } from "../utils/WeatherUtils";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     "& .MuiCard-root": {
       backgroundColor: "transparent",
       backdropFilter: "blur(1px)",
-      width: "80%",
-      height: "60%",
-      maxWidth: "1000px",
-      minWidth: "300px",
-      minHeight: "300px",
-      display: "grid",
       borderRadius: "30px",
     },
     display: "flex",
@@ -25,81 +20,53 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     height: "84.5vh",
   },
+  card: {
+    width: "80%",
+    height: "60%",
+    maxWidth: "1000px",
+    minWidth: "300px",
+    minHeight: "300px",
+    display: "grid",
+  },
+  content: {
+    display: "flex",
+    justifyContent: "space-evenly",
+  },
 }));
-
-const envApiKeys = process.env.REACT_APP_API_KEYS;
-const weatherApiKeys = envApiKeys.split(",");
-const baseUrl = "https://localhost:7061/api/WeatherInformation/getWeatherInfo/";
-let currentIndex = 0;
 
 export default function ContainerCard() {
   const classes = useStyles();
   const [weather, setWeather] = useState(null);
   const [error, setError] = useState(null);
-  const apiKey = weatherApiKeys[currentIndex];
   const [loading, setLoading] = useState(false);
 
-  function search(city, country) {
+  async function handleSearch(city, country) {
     setWeather(null);
     setError(null);
     setLoading(true);
-    const headers = { "x-api-key": `${apiKey}` };
-    console.log(
-      `Requesting weather info for ${city}/${country} using API key ${apiKey}`
-    );
-    fetch(`${baseUrl}${city}/${country}`, { headers })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else if (res.status === 429) {
-          if (currentIndex < weatherApiKeys.length - 1) {
-            currentIndex++;
-            console.log(
-              `API key ${apiKey} has reached its request limit. Switching to API key ${weatherApiKeys[currentIndex]}`
-            );
-          } else {
-            currentIndex = 0;
-            throw new Error(
-              "All API keys have reached their request limit. Please wait an hour to make another request."
-            );
-          }
-          throw new Error(
-            "Current API key has reached its request limit. Please try again to use another API key for your request."
-          );
-        } else if (res.status === 404) {
-          throw new Error(
-            "City not found. Please enter a valid City and Country"
-          );
-        } else {
-          throw new Error("Unable to retrieve weather data. Please try again.");
-        }
-      })
-      .then((data) => {
-        setError(null);
-        setWeather(data);
 
-        console.log(weather);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if (error instanceof TypeError) {
-          setError(
-            new Error("Problem connecting to server. Please try again later.")
-          );
-        } else {
-          console.error(error);
-          setWeather(null);
-          setError(error);
-        }
-      })
-      .finally(() => setLoading(false));
+    try {
+      const data = await fetchWeather(city, country);
+      setWeather(data);
+    } catch (error) {
+      if (error instanceof TypeError) {
+        setError(
+          new Error("Problem connecting to server. Please try again later.")
+        );
+      } else {
+        setError(error);
+      }
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
     <div className={classes.root}>
-      <Card variant="outlined">
+      <Card variant="outlined" className={classes.card}>
         <CardContent
-          style={{ display: "flex", justifyContent: "space-evenly" }}
+          data-testid="cardcontent"
+          className={classes.content}
         >
           {weather && weather.weather && weather.weather[0].iconSource && (
             <WeatherInfoArea
@@ -116,7 +83,7 @@ export default function ContainerCard() {
               <DotLoader color={"#474e68"} loading={loading} size={80} />
             </div>
           )}
-          <SearchArea onSearch={search} />
+          <SearchArea onSearch={handleSearch} />
         </CardContent>
       </Card>
     </div>
---
+++ src/utils/WeatherUtils.js
@@ -0,0 +1,41 @@
+const baseUrl = "https://localhost:7061/api/WeatherInformation/getWeatherInfo/";
+const envApiKeys = process.env.REACT_APP_API_KEYS;
+const weatherApiKeys = envApiKeys.split(",");
+let currentIndex = 0;
+const apiKey = weatherApiKeys[currentIndex];
+
+export async function fetchWeather(city, country) {
+  const headers = { "x-api-key": apiKey };
+  const url = `${baseUrl}${city}/${country}`;
+
+  try {
+    const response = await fetch(url, { headers });
+
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+
+    if (response.status === 429) {
+      if (currentIndex < weatherApiKeys.length - 1) {
+        currentIndex++;
+      } else {
+        currentIndex = 0;
+        throw new Error(
+          "All API keys have reached their request limit. Please wait an hour to make another request."
+        );
+      }
+      throw new Error(
+        "Current API key has reached its request limit. Please try again to use another API key for your request."
+      );
+    }
+
+    if (response.status === 404) {
+      throw new Error("City not found. Please enter a valid City and Country");
+    }
+
+    throw new Error("Unable to retrieve weather data. Please try again.");
+  } catch (error) {
+    throw error;
+  }
+}
